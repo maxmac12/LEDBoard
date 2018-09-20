1a8f4697079facd12d828326975a0232cbcab7b4
Max MacCamy <maxmac12@gmail.com>
2018-09-19 17:11:59 -0700

    Added supported commands to the about command

--- console/AboutCommand.cpp
+++ console/AboutCommand.cpp
@@ -29,6 +29,15 @@ AboutCommand::AboutCommand()
 ErrorCode AboutCommand::exec(const CStr subject, const CStr value1, const CStr value2, const CStr value3)
 {
     maintComm->sendData("LEDBoard Software\r\n");
+    maintComm->sendData("Developed by Max MacCamy, 2018\r\n");
+    maintComm->sendData("Supported Commands:\r\n");
+    maintComm->sendData("   mode off\r\n");
+    maintComm->sendData("   mode white\r\n");
+    maintComm->sendData("   mode rainbow\r\n");
+    maintComm->sendData("   mode wrainbow [wLength] [delay (ms)]\r\n");
+    maintComm->sendData("   mode color [r] [g] [b]\r\n");
+    maintComm->sendData("   mode pulse [delay (ms)]\r\n");
+    maintComm->sendData("   stat\r\n");
 
     return (ER_SUCCESS);
 }
